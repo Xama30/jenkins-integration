cfff7ed738be813943e5e214674bd8e711eed549
Xama30 <xavier.breton30@gmail.com>
2026-03-27 15:44:02 -0400

    test

--- ServeurTracker.Api/Models/Device.cs
+++ ServeurTracker.Api/Models/Device.cs
@@ -3,11 +3,12 @@ namespace ServeurTracker.Api.Models
     public class Device
     {
         public int Id { get; set; }
-        
+
         public string Name { get; set; } = string.Empty;
-        
+
         public string IpAddress { get; set; } = string.Empty;
-        
+
         public bool IsOnline { get; set; }
     }
 }
+//TEST CHECK
